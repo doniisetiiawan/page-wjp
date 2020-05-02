20fe07ebba08357e97503339cc1f32fde27e856c
Doni Setiawan <donisetiawan.050@gmail.com>
2020-05-02 14:25:55 +0700

    Using a router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import './index.css';
 import './example.css';
+import { createBrowserHistory } from 'history';
 import App from './page-admin';
 import Backend from './backend';
 import * as serviceWorker from './serviceWorker';
 
+const history = createBrowserHistory();
+
 const backend = new Backend();
+
+const Nav = (props) => (
+  <ol className="nav">
+    {props.pages.map((page) => (
+      <li>
+        <Link to={`pages/${page.id}`} replace>
+          {page.title}
+        </Link>
+      </li>
+    ))}
+  </ol>
+);
+
+const StaticPage = (props) => {
+  const id = props.match.params.page || 1;
+
+  const pages = backend
+    .all()
+    .filter((page) => page.id == id);
+
+  if (pages.length < 1) {
+    return <div>not found</div>;
+  }
+
+  return (
+    <div className="page">
+      <h1>{pages[0].title}</h1>
+      {pages[0].body}
+      <Link to="/"> Back</Link>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App backend={backend} />
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact>
+          <Nav pages={backend.all()} />
+          <App backend={backend} />
+        </Route>
+        <Route
+          path="/pages/:page"
+          render={(props) => (<StaticPage {...props} backend={backend} />
+          )}
+        />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
